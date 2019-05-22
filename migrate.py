423d5a6c5c3bb9d1db27503b820cd74229f8a26d
jazevedo6 <jazevedo6@gatech.edu>
2019-05-22 13:44:04 -0400

    Add most of the functionality

--- migrate.py
+++ migrate.py
@@ -1,71 +1,213 @@
-import sys
 import os
 import pexpect
+import argparse
+import shutil
+from pathlib import Path
 
 __author__ = "Joseph Azevedo"
+__version__ = "1.0"
 
-"""
-migrate.py
-----------
-TODO write
-"""
-
-HELP = """
-migrate.py
-Usage:
-   migrate.py TODO
-
-Options:
-  -h --help    Show this help description"""
-
-TEMP_DIR = "temp"
-BASE_CLONE_CMD = "git clone {} --bare"
+DESCRIPTION = "Migrate github repositories between two different optionally authenticated remotes."
+DEFAULT_TEMP_DIR = "temp"
+DEFAULT_REMOTE = "public"
+DEFAULT_TIMEOUT = 10000
 
 
-def main():
-    if '--help' in sys.argv or '-h' in sys.argv:
-        help()
-    elif len(sys.argv) < 3:
-        print("Wrong number of arguments. See usage:")
-        help()
+def main(source_repo, dest_repo, source_auth=None, dest_auth=None, temp_dir=None, remote=None, timeout=None):
+    """Performs the migration logic from source_repo to dest_repo, creating a temporary directory (if it doesn't
+    already exist) and bare cloning into that. Then, the dest repo is added as a remote to the local copy before
+    pushing to that remote. Finally, the repo folder is deleted, and if the temporary directory didn't exist before,
+    it is also removed.
 
-    source_repo = sys.argv[1]
-    dest_repo   = sys.argv[2]
+    :param source_repo: The https path of the source repository
+    :param dest_repo:   The https path of the destination repository
+    :param source_auth: An optional tuple containing (user, password/token) for the source repo
+    :param dest_auth:   An optional tuple containing (user, password/token) for the destination repo
+    :param temp_dir:    An optional path to use as the temporary directory to clone repos into (./temp used otherwise)
+    :param remote       An optional name for the remote
+    :param timeout      Optional ms to wait between git command parses in pexpect
+    """
+    if timeout is None:
+        timeout = DEFAULT_TIMEOUT
 
+    temp_path, temp_existed_before = try_create_temp_dir(temp_dir)
+    repo_path = clone_bare(source_repo, temp_path, source_auth, timeout=timeout)
+    remote_name = add_remote(repo_path, dest_repo, remote)
+    push_mirror(repo_path, remote_name, timeout=timeout)
+
+    # Clean up
     try:
-        # Create target Directory
-        os.mkdir(TEMP_DIR)
-        print("Temporary dir {} created ".format(TEMP_DIR))
-    except FileExistsError:
-        print("Temporary dir {} already exists ".format(TEMP_DIR))
-
-    new_cwd = os.path.join(os.getcwd(), TEMP_DIR)
-
-    command = BASE_CLONE_CMD.format(source_repo)
-    process = pexpect.spawn(command, cwd=new_cwd)
-    process.timeout = 300
-    process.expect(r"Cloning into bare repository '(.+)'...[\n\r]+")
-    print(process.match.group(0))
-    repo_name = process.match.group(0)
-
-    result = process.expect(['Unpacking objects: [0-9]+% ([0-9]+/[0-9]+), done.', 'fatal: '])
-    if result == 0:
-        print('Bare clone succeeded')
-
-    elif result == 1:
-        print('An error occurred: {}'.format(process.after))
-        process.kill(0)
+        if temp_existed_before:
+            shutil.rmtree(repo_path)
+        else:
+            shutil.rmtree(temp_path)
+    except OSError as e:
+        print ("An error ocurred in cleanup. Exiting")
         quit()
 
-    new_cwd = os.path.join(new_cwd, repo_name)
+
+def clone_bare(source_repo, path, source_auth=None, timeout=DEFAULT_TIMEOUT):
+    base_clone_command = """git clone {} --bare"""
+    command = base_clone_command.format(source_repo)
+    process = pexpect.spawn(command, cwd=path)
+    process.timeout = timeout
+
+    # Can fail if not valid
+    result = process.expect([r"Cloning into bare repository '(.+)'[.][.][.][\n\r]+", "fatal: "])
+    if result == 1:
+        error(process)
+
+    # Succeeded
+    repo = process.match.group(1).decode("utf-8")
+    print("""Attempting to clone repository "{}" """.format(repo))
+
+    # Potential login stage
+    success_pattern = r"Unpacking objects: [0-9]+% [(][0-9]+/[0-9]+[)], done[.]"
+    if handle_auth(process, success_pattern, source_auth):
+        repo_path = (Path(path) / repo).resolve()
+        print("""Bare repository successfully cloned into "{}" """.format(repo_path))
+        return repo_path
+    else:
+        return ""
 
 
-def help():
-    # Display help and stop
-    print(HELP)
+def add_remote(repo_path, remote_repo, remote_name=None):
+    """Adds a remote to the given repository"""
+    if remote_name is None:
+        remote_name = DEFAULT_REMOTE
+
+    base_add_remote_command = """git remote add {} {}"""
+    command = base_add_remote_command.format(remote_name, remote_repo)
+    process = pexpect.spawn(command, cwd=repo_path)
+    
+    process.expect(pexpect.EOF)
+    process.kill(0)
+    print("""Remote "{}" added""".format(remote_name))
+    return remote_name
+
+
+def push_mirror(repo_path, dest_auth, remote_name, timeout=DEFAULT_TIMEOUT):
+    """Pushes the repository at the given location to the specified remote as a mirror"""
+    base_push_command = """git push {} --mirror"""
+    command = base_push_command.format(remote_name)
+    process = pexpect.spawn(command, cwd=repo_path)
+    process.timeout = timeout
+
+    success_pattern = r"remote: Resolving deltas: [0-9]+% [(][0-9]+/[0-9]+[)], done[.]"
+    if handle_auth(process, success_pattern, dest_auth):
+        print("""Pushed to remote "{}" """.format(remote_name))
+
+
+def handle_auth(process, success_pattern, auth=None):
+    error_pattern = "fatal: "
+    result = process.expect([success_pattern, error_pattern, pexpect.EOF, """Username for '(.+)':"""])
+    if result == 3:
+        server = process.match.group(1).decode("utf-8")
+        # Authentication required
+        if auth is not None:
+            process.sendline(auth[0])
+            process.expect("""Password for '.+':""")
+            process.sendline(auth[1])
+            print("""Authenticated for repo at "{}" """.format(server))
+            result = process.expect([success_pattern, error_pattern, pexpect.EOF])
+        else:
+            print("Error: Authentication required for \"{}\" but insufficient credentials were supplied"
+                .format(server))
+            exitScript(process)
+
+    if result == 0:
+        process.expect(pexpect.EOF)
+        process.kill(0)
+        return True
+    elif result == 1:
+        error(process)
+    elif result == 2:
+        error(process, at_eof=True)
+    return False
+
+
+def error(process, at_eof=False):
+    """Handles a fatal error and closes a pexpect process before terminating the script"""
+    error_format = """An unexpected error occurred: "{}" """
+
+    if not at_eof:
+        process.expect(pexpect.EOF)
+        
+    error_text = before(process)
+    print(error_format.format(error_text))
+    exitScript(process)
+
+
+def exitScript(process=None):
+    if process is not None:
+        process.kill(0)
     quit()
+
+
+def before(process):
+    """Utility method to strip and decode a pexpect process' before output"""
+    return process.before.decode("utf-8").strip()
+
+
+def try_create_temp_dir(path):
+    """Creates temporary directory, returning (path of temporary directory, whether the directory already existed)
+
+    Defaults to 'temp'"""
+    if path is None:
+        path = DEFAULT_TEMP_DIR
+
+    resolved = resolve_path(path)
+    if try_make_dir(resolved):
+        print("""Temporary dir "{}" created""".format(resolved))
+        return resolved, False
+    else:
+        print("""Temporary dir "{}" already exists""".format(resolved))
+        return resolved, True
+
+
+def resolve_path(path):
+    """Resolves an absolute/relative path to an absolute one (relative to current file's parent if applicable)"""
+    if not os.path.isabs(path):
+        return (Path(__file__).parent / path).resolve()
+    else:
+        return path
+
+
+def try_make_dir(path):
+    """Tries to make the directory at the path, and returns true if it succeeded and false if the file already existed
+    (other IO errors are unhandled)"""
+    try:
+        os.mkdir(path)
+        return True
+    except FileExistsError:
+        return False
+
+
+def construct_non_none_tuple(a, b):
+    """Returns a tuple of two non-none values, or None if either arguments are None"""
+    if a is not None and b is not None:
+        return a, b
+    else:
+        return None
 
 
 # Run script
 if __name__ == "__main__":
-    main()
+    # Argument definitions
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('source',        metavar='sourceRepo', help='the source github repository')
+    parser.add_argument('dest',          metavar='destRepo',   help='the destination github repository')
+    parser.add_argument('--sourceUser',  metavar='user',       help='authentication user for the source repo')
+    parser.add_argument('--sourceToken', metavar='token',      help='authentication token/password for the source repo')
+    parser.add_argument('--destUser',    metavar='user',       help='authentication user for the dest repo')
+    parser.add_argument('--destToken',   metavar='token',      help='authentication token/password for the dest repo')
+    parser.add_argument('--temp',        metavar='path',       help='temp directory for cloning the source repo')
+    parser.add_argument('--remote',      metavar='name',       help='name of the destination remote to use')
+    parser.add_argument('--timeout',     metavar='ms',         help='max amount of time to wait between command parses')
+
+    # Parse arguments
+    args = parser.parse_args()
+    sourceAuth = construct_non_none_tuple(args.sourceUser, args.sourceToken)
+    destAuth   = construct_non_none_tuple(args.destUser,   args.destToken)
+    main(args.source, args.dest, source_auth=sourceAuth, dest_auth=destAuth,
+        temp_dir=args.temp, remote=args.remote, timeout=args.timeout)
